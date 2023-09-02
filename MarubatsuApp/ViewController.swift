84ef4f9261d3d5a3a9b0160e0e5b2da60c323b5e
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2023-09-02 10:18:33 +0900

    3kaime

--- MarubatsuApp/ViewController.swift
+++ MarubatsuApp/ViewController.swift
@@ -8,17 +8,53 @@
 import UIKit
 
 
-class ViewController: UIViewController() {
+class ViewController: UIViewController {
+    
     @IBOutlet weak var questionLabel: UILabel!
+    // 表示中の問題番号を格納
+    var currentQuestionNum: Int = 0
     
-
+    // 問題
+      let questions: [[String: Any]] = [
+          [
+              "question": "iPhoneアプリを開発する統合環境はZcodeである",
+              "answer": false
+          ],
+          [
+              "question": "Xcode画面の右側にはユーティリティーズがある",
+              "answer": true
+          ],
+          [
+              "question": "UILabelは文字列を表示する際に利用する",
+              "answer": true
+          ]
+      ]
     
-}
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func showQuestion() {
+          let question = questions[currentQuestionNum]
+
+          if let que = question["question"] as? String {
+              questionLabel.text = que
+          }
+      }
+    }
+    
+    
+    
+    
+    
+    @IBAction func tappendButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tappendYesBitton(_ sender: UIButton) {
+    }
+
+
 
 
 }
