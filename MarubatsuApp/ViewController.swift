f2abdbe2b6795dfadc7517a65aa83dc833172e93
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2023-09-02 11:22:18 +0900

    ループしてる成功コミット

--- MarubatsuApp/ViewController.swift
+++ MarubatsuApp/ViewController.swift
@@ -15,20 +15,20 @@ class ViewController: UIViewController {
     var currentQuestionNum: Int = 0
     
     // 問題
-      let questions: [[String: Any]] = [
-          [
-              "question": "iPhoneアプリを開発する統合環境はZcodeである",
-              "answer": false
-          ],
-          [
-              "question": "Xcode画面の右側にはユーティリティーズがある",
-              "answer": true
-          ],
-          [
-              "question": "UILabelは文字列を表示する際に利用する",
-              "answer": true
-          ]
-      ]
+    let questions: [[String: Any]] = [
+        [
+            "question": "iPhoneアプリを開発する統合環境はZcodeである",
+            "answer": false
+        ],
+        [
+            "question": "Xcode画面の右側にはユーティリティーズがある",
+            "answer": true
+        ],
+        [
+            "question": "UILabelは文字列を表示する際に利用する",
+            "answer": true
+        ]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,43 +41,46 @@ class ViewController: UIViewController {
         
         if let que = question["question"] as? String {
             questionLabel.text = que
-            
-            
         }
-        // 回答をチェックする関数
-        // 回答チェックの関数、正解なら次の問題を表示
-        func checkAnswer(yourAnswer: Bool) {
-            let question = questions[currentQuestionNum]
-            
-            if let ans = question["answer"] as? Bool {
-                
-                if yourAnswer == ans {
-                    // 正解
-                    // currentQuestionNumを1足して次の問題に進む
-                    currentQuestionNum += 1
-                } else {
-                    // 不正解
-                    
-                }
-            } else {
-                print("答えが入ってません")
-                return
-            }
-            
-            // 問題を表示します。
-            // 正解であれば次の問題が、不正解であれば同じ問題が再表示されます。
-            showQuestion()
-            
-        }
-        
     }
     
-    
+    // 回答をチェックする関数
+    // 回答チェックの関数、正解なら次の問題を表示
+    func checkAnswer(yourAnswer: Bool) {
+        let question = questions[currentQuestionNum]
+        
+        if let ans = question["answer"] as? Bool {
+            
+            if yourAnswer == ans {
+                // 正解のときの処理
+                // currentQuestionNumを1足して次の問題に進む 配列の1番目が呼び出される
+                currentQuestionNum += 1
+            } else {
+                // 不正解のときの処理
+                
+            }
+        } else {
+            print("答えが入ってません")
+            return
+        }
+        // currentQuestionNumの値が問題数以上だったら最初の問題に戻す
+              if currentQuestionNum >= questions.count {
+                  currentQuestionNum = 0
+              }
+        
+        // 問題を表示します。
+        // 正解であれば次の問題が、不正解であれば同じ問題が再表示されます。
+        showQuestion()
+        
+    }
+
     
     @IBAction func tappendButton(_ sender: UIButton) {
+        checkAnswer(yourAnswer: false)
     }
     
     @IBAction func tappendYesBitton(_ sender: UIButton) {
+        checkAnswer(yourAnswer: true)
     }
     
     
